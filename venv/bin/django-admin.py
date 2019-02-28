cc51d08171dbd1688113226908a04bcdef86f371
Ran Xuebin <31369028+ranxuebin@users.noreply.github.com>
2019-02-28 23:06:16 +0800

    Django init

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mac/Project/PycharmProjects/Conference_room_management_system/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
